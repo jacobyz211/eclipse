2787415edf045499f36e62aad1f8d43b2173c162
jacobyz211 <cyrusna29@icloud.com>
2026-03-30 04:55:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,48 +6,46 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# This is the "Home" page so you can see if the server is alive
 @app.route('/')
 def home():
-    return "YouTube Music Addon is LIVE! Go to /manifest.json to see the data."
+    return "SoundCloud Bridge is LIVE! Use /manifest.json in Eclipse."
 
-# This is the file Eclipse Music looks for
 @app.route('/manifest.json')
 def manifest():
     return jsonify({
-        "id": "com.cyrus.ytm",
-        "name": "YouTube Music Bridge",
+        "id": "com.cyrus.sc",
+        "name": "SoundCloud Bridge",
         "version": "1.0.0",
         "resources": ["search", "stream"],
         "types": ["track"]
     })
 
-# This handles the searching
 @app.get('/search')
 def search():
     query = request.args.get('q', '')
-    ydl_opts = {'quiet': True, 'default_search': 'ytsearch5', 'format': 'bestaudio/best'}
+    # We use 'scsearch' instead of 'ytsearch'
+    ydl_opts = {'quiet': True, 'default_search': 'scsearch5'}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(f"ytsearch:{query} topic", download=False)
+        info = ydl.extract_info(f"scsearch:{query}", download=False)
         tracks = []
         for e in info.get('entries', []):
             tracks.append({
-                "id": e['id'],
+                "id": e.get('url'), # SoundCloud uses the full URL as ID
                 "title": e.get('title'),
-                "artist": e.get('uploader'),
+                "artist": e.get('uploader', 'SoundCloud Artist'),
                 "duration": e.get('duration'),
                 "artworkURL": e.get('thumbnail'),
-                "format": "m4a"
+                "format": "mp3"
             })
         return jsonify({"tracks": tracks})
 
-# This resolves the playable link
-@app.get('/stream/<track_id>')
-def stream(track_id):
+@app.get('/stream')
+def stream():
+    track_url = request.args.get('id')
     ydl_opts = {'quiet': True, 'format': 'bestaudio/best'}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(f"https://www.youtube.com/watch?v={track_id}", download=False)
-        return jsonify({"url": info['url'], "format": "m4a"})
+        info = ydl.extract_info(track_url, download=False)
+        return jsonify({"url": info['url'], "format": "mp3"})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
